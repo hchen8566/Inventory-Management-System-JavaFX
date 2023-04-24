85344de169454209a5d948d661fdee339c4170c4
hc7715 <hchen3@clemson.edu>
2023-04-23 23:44:59 -0400

    Models for part, inventory, and product implemented

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,22 @@
+package main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 
-
-
-
-
-
-public class Main {
+public class Main extends  Application{
     public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
+        launch();
         }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/MainForm.fxml"));
+        stage.setTitle("Inventory Management System");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
